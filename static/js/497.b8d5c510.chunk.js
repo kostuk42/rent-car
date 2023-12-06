6483eef81454fde5395220f3993c729359c638e6
kostiuchenkoo <kostiuchenkoo@upmc.edu>
2023-12-06 18:22:21 +0200

    Updates

---
+++ static/js/497.b8d5c510.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkrent_car=self.webpackChunkrent_car||[]).push([[497],{2497:(e,a,s)=>{s.r(a),s.d(a,{default:()=>c});s(2791);var t=s(5048),r=s(6170),n=s(184);const c=()=>{const e=(0,t.v9)((e=>e.favorites));return(0,n.jsxs)("div",{className:"catalog",children:[e&&e.map(((e,a)=>(0,n.jsx)(r.Z,{advert:e},a))),!(null!==e&&void 0!==e&&e.length)&&(0,n.jsx)("p",{style:{paddingTop:"29px"},children:"No favorite adverts yet."})]})}}}]);
+//# sourceMappingURL=497.b8d5c510.chunk.js.map
